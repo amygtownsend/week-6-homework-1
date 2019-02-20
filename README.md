dd0b86ad2b02c813ec72f8d35650217488d71416
Glitch (rebel-son) <none>
2019-02-20 23:33:49 +0000

    Import of amygtownsend/week-6-homework-1

--- README.md
+++ README.md
@@ -1,27 +1,75 @@
-Welcome to Glitch
-=================
+Spotify Playground
+=========================
 
-Click `Show` in the header to see your app live. Updates to your code will instantly deploy and update live.
+Written by [Arielle Vaniderstine](https://twitter.com/imariari)
 
-**Glitch** is the friendly community where you'll build the app of your dreams. Glitch lets you instantly create, remix, edit, and host an app, bot or site, and you can invite collaborators or helpers to simultaneously edit code with you.
+A basic Node app that integrates with the Spotify API. Explore different endpoints the API has to offer!
 
-Find out more [about Glitch](https://glitch.com/about).
+This app uses the *Client Credentials Flow* for authentication, which means you can only get non-user-specific data.
 
+-----
 
-Your Project
-------------
+## Getting Started
 
-On the front-end,
-- edit `public/client.js`, `public/style.css` and `views/index.html`
-- drag in `assets`, like images or music, to add them to your project
+1. After registering your app at developer.spotify.com/dashboard, put your app's client ID and secret (which you can find in the Dashboard) into the `.env` file.
 
-On the back-end,
-- your app starts at `server.js`
-- add frameworks and packages in `package.json`
-- safely store app secrets in `.env` (nobody can see this but you and people you invite)
+2. Click on "Show Live" in Glitch and verify that your app works (you should see data in each of the coloured sections).
 
+-----
 
-Made by [Glitch](https://glitch.com/)
--------------------
+## Working with the Playground
 
-\ ゜o゜)ノ
+- Navigate between files in Glitch's left panel.
+- See server logs by clicking "Status" at the top of the left panel.
+- Open the browser's **Developer Tools** by right-clicking and selecting **Inspect**.
+- Calls to the Spotify API live in **server.js**.
+- Calls to your own server and the rendering of the UI occur in **public/client.js**.
+
+-----
+
+## Challenges
+
+#### Pink: Search for a Track
+
+1. Change the song that shows up. *(Hint: look in server.js for the API call)*
+
+2. Display the artist name in addition to the song name. *(Hint: look at the whole track object to find the artist name)*
+
+3. Make the title link to the song on Spotify. *(Hint: look at the whole track object to find the song URL)*
+
+#### Purple: Get a Category's Playlists
+
+1. Get the playlists for a different category! You can find other categories [here](https://beta.developer.spotify.com/console/get-browse-categories/)
+
+2. Show 10 playlists instead of 5.
+
+3. Different playlists are shown to users in different countries. Show the playlists from another country.
+
+#### Orange: Get Audio Features for a Track
+
+1. What track are these audio features for?
+
+2. There are more audio features available. Display 2 more features.
+
+3. Get the audio features for another track.
+
+#### Blue: Get an Artist
+
+1. Get another artist.
+
+2. Show the popularity value for the artist.
+
+3. Show the number of followers the artist has.
+
+#### Yellow: Get an Artist's Top Tracks
+
+1. Whose top tracks are these?
+
+2. Get the top tracks of another artist.
+
+3. Top Tracks vary by country. Which country are these top tracks for? Show the top tracks in another country.
+
+### Bonus!
+
+Use a different endpoint to get totally different data! *(Hint: look at [this repository](https://github.com/thelinmichael/spotify-web-api-node/) for examples of all the calls you can make)*
+
--- public/client.js
+++ public/client.js
@@ -1,42 +1,107 @@
 // client-side js
 // run by the browser each time your view template is loaded
 
-console.log('hello world :o');
+$(function() {
+    
+  fetch('/search-track').then(resp => resp.json()).then((data) => {
+    // "Data" is the object we get from the API. See server.js for the function that returns it.
+    console.group('%cResponse from /search-track', 'color: #F037A5; font-size: large');
+    console.log(data);
+    console.groupEnd();
+    
+    // Display the track name
+    var trackName = $(
+      // '<h3><a href="' + data.external_urls.spotify + '" target="blank">' + data.name + '</a></h3>'
+      `<h3><a href="${data.external_urls.spotify}">${data.name}</a></h3>`
+    );
+    trackName.appendTo('#search-track-container');
+    
+    // Display the artist name
+    var artists = '';
+    
+    data.artists.forEach(function(item) {
+      artists = artists + item.name + ' ';
+    });
+    
+    let h5 = document.createElement('h5');
+    h5.innerText = artists;
+    document.getElementById('search-track-container').append(h5);
+    
+    // Display the album art
+    var img = $('<img/>');
+    img.attr('src', data.album.images[0].url);
+    img.appendTo('#search-track-container');
+  });
+  
+  $.get('/category-playlists', function(data) {
+    // "Data" is the object we get from the API. See server.js for the function that returns it.
+    console.group('%cResponse from /category-playlists', 'color: #F037A5; font-size: large');
+    console.log(data);
+    console.groupEnd();
+    
+    // Display the covers of the playlists
+    data
+      .forEach((c) => {
+      $('#category-playlists-container').append(`<br><h1>${c.name}</h1><br>`)
+      c.data.playlists.items.map(function(playlist, i) {
+      var img = $('<img class="cover-image"/>');
+      img.attr('src', playlist.images[0].url);
+      img.appendTo('#category-playlists-container');
+    });
+    })
+  });
+  
+  $.get('/audio-features', function(data) {
+    // "Data" is the object we get from the API. See server.js for the function that returns it.
+    console.group('%cResponse from /audio-features', 'color: #F037A5; font-size: large');
+    console.log(data);
+    console.groupEnd();
+    
+    // The audio features we want to show
+    var keys = ["danceability", "energy", "acousticness", "speechiness", "loudness"]
+    
+    // Display the audio features
+    keys.map(function(key, i) {
+      if (data.hasOwnProperty(key)) {
+        var feature = $('<p><span class="big-number">' + data[key] + ' </span>'  + key + '</p>');
+        feature.appendTo('#audio-features-container');
+      }
+    });
+  });
+  
+  $.get('/artist', function(data) {
+    // "Data" is the object we get from the API. See server.js for the function that returns it.
+    console.group('%cResponse from /artist', 'color: #F037A5; font-size: large');
+    console.log(data);
+    console.groupEnd();
+    
+    // Display the artist's image
+    var img = $('<img class="circle-image" />');
+    img.attr('src', data.images[0].url);
+    img.appendTo('#artist-container');
+    
+    // Display the artist name
+    var trackName = $('<h3>' + data.name + '</h3>');
+    trackName.appendTo('#artist-container');
+    
+    // Display the artist's genres
+    data.genres.map(function(genre, i) {
+      var genreItem = $('<p>' + genre + '</p>');
+      genreItem.appendTo('#artist-container');
+    });
+  });
+  
+  $.get('/artist-top-tracks', function(data) {
+    // "Data" is the object we get from the API. See server.js for the function that returns it.
+    console.group('%cResponse from /artist-top-tracks', 'color: #F037A5; font-size: large');
+    console.log(data);
+    console.groupEnd();
+    
+    // Display the audio features
+    data.map(function(track, i) {
+      var trackName = $('<li>' + track.name + '</li>');
+      trackName.appendTo('#top-tracks-container');
+    });
+  });
 
-// our default array of dreams
-const dreams = [
-  'Find and count some sheep',
-  'Climb a really tall mountain',
-  'Wash the dishes'
-];
-
-// define variables that reference elements on our page
-const dreamsList = document.getElementById('dreams');
-const dreamsForm = document.forms[0];
-const dreamInput = dreamsForm.elements['dream'];
-
-// a helper function that creates a list item for a given dream
-const appendNewDream = function(dream) {
-  const newListItem = document.createElement('li');
-  newListItem.innerHTML = dream;
-  dreamsList.appendChild(newListItem);
-}
-
-// iterate through every dream and add it to our page
-dreams.forEach( function(dream) {
-  appendNewDream(dream);
 });
-
-// listen for the form to be submitted and add a new dream when it is
-dreamsForm.onsubmit = function(event) {
-  // stop our form submission from refreshing the page
-  event.preventDefault();
-
-  // get dream value and add it to the list
-  dreams.push(dreamInput.value);
-  appendNewDream(dreamInput.value);
-
-  // reset form 
-  dreamInput.value = '';
-  dreamInput.focus();
-};
