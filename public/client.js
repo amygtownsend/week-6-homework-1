d02668119cdb17e4d265ba745491cc138b1788ff
Glitch (rebel-son) <none>
2019-02-21 00:58:36 +0000

    Rewrite all jQuery in vanilla JS

--- public/client.js
+++ public/client.js
@@ -74,50 +74,63 @@ document.addEventListener("DOMContentLoaded", function() {
     
     // The audio features we want to show
     let keys = ["danceability", "energy", "acousticness", "speechiness", "loudness"];
+    let audioFeatures = document.getElementById('audio-features-container');
     
     // Display the audio features
     keys.map(function(key, i) {
       if (data.hasOwnProperty(key)) {
         let p = document.createElement('p');
-        p.innerHTML = `<span class`
-        var feature = $('<p><span class="big-number">' + data[key] + ' </span>'  + key + '</p>');
-        feature.appendTo('#audio-features-container');
+        p.innerHTML = `<span class="big-number">${data[key]}</span>${key}</p>`;
+        // var feature = $('<p><span class="big-number">' + data[key] + ' </span>'  + key + '</p>');
+        audioFeatures.appendChild(p);
       }
     });
   });
   
-  $.get('/artist', function(data) {
+  fetch('/artist').then(resp => resp.json()).then((data) => {
     // "Data" is the object we get from the API. See server.js for the function that returns it.
     console.group('%cResponse from /artist', 'color: #F037A5; font-size: large');
     console.log(data);
     console.groupEnd();
     
     // Display the artist's image
-    var img = $('<img class="circle-image" />');
-    img.attr('src', data.images[0].url);
-    img.appendTo('#artist-container');
+    let img = document.createElement('img');
+    let imgClass = document.createAttribute('class');
+    imgClass.value = "circle-image";
+    let src = document.createAttribute('src');
+    src.value = data.images[0].url;
+    img.setAttributeNode(imgClass);
+    img.setAttributeNode(src);
+    
+    let artist = document.getElementById('artist-container');
+    artist.appendChild(img);
     
     // Display the artist name
-    var trackName = $('<h3>' + data.name + '</h3>');
-    trackName.appendTo('#artist-container');
+    let h3 = document.createElement('h3');
+    h3.innerHTML = data.name;
+    artist.appendChild(h3);
     
     // Display the artist's genres
     data.genres.map(function(genre, i) {
-      var genreItem = $('<p>' + genre + '</p>');
-      genreItem.appendTo('#artist-container');
+      let p = document.createElement('p');
+      p.innerHTML = genre;
+      artist.appendChild(p);
     });
   });
   
-  $.get('/artist-top-tracks', function(data) {
+  fetch('/artist-top-tracks').then(resp => resp.json()).then((data) => {
     // "Data" is the object we get from the API. See server.js for the function that returns it.
     console.group('%cResponse from /artist-top-tracks', 'color: #F037A5; font-size: large');
     console.log(data);
     console.groupEnd();
     
+    let topTracks = document.getElementById('top-tracks-container');
+    
     // Display the audio features
     data.map(function(track, i) {
-      var trackName = $('<li>' + track.name + '</li>');
-      trackName.appendTo('#top-tracks-container');
+      let li = document.createElement('li');
+      li.innerHTML = track.name;
+      topTracks.appendChild(li);
     });
   });
 
